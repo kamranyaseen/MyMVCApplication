b161a37c023bb10e00aa6b45d819c64aaf032272
Kamran Yaseen <kamranyaseen@hotmail.com>
2020-09-16 03:56:03 +0300

    test development

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using MyMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        static IList<Student> studentList = new List<Student>
+            {
+                new Student(){StudentID=1,Name="Kamran Yaseen",Age=21},
+                new Student(){StudentID=2,Name="Imran Yaseen",Age=23},
+                new Student(){StudentID=3,Name="Yousuf Yaseen",Age=23},
+                new Student(){StudentID=4,Name="Azhar Yaseen",Age=26},
+                new Student(){StudentID=5,Name="Sana Yaseen",Age=12 },
+                new Student(){StudentID=6,Name="Aiman Saleem",Age=32},
+                new Student(){StudentID=7,Name="Ahlam TRemimi",Age=53},
+                new Student(){StudentID=8,Name="Ibrahim Yaseen",Age=53},
+                new Student(){StudentID=9,Name="Aamir Sattar",Age=34},
+                new Student(){StudentID=10,Name="Abdullah Khan",Age=23}
+            };
+        public ActionResult Index()
+        {
+            return View(studentList.OrderBy(s=>s.StudentID).ToList());
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var std = studentList.Where(s => s.StudentID == Id).FirstOrDefault();
+
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = studentList.Where(s => s.StudentID == std.StudentID).FirstOrDefault();
+                studentList.Remove(std);
+                studentList.Add(std);
+                return RedirectToAction("Index");
+            }
+            return View("Index");
+           
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApplication.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "Name field is required")]
+        public String Name { get; set; }
+
+        [Range(18,55,ErrorMessage = "Age field is required betweeen 18 to 55.")]
+        public int Age { get; set; }
+
+        public Standard standard { get; set; }
+
+    }
+
+    public class Standard
+    {
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+    }
+}
